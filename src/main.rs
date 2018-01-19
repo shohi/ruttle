26d8f0d6ab6c3cdff4491c8fbac72acfddc628a6
shohi <oshohi@gmail.com>
2018-01-19 17:59:59 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+// use std::path::Path;
+use std::fs;
+
+const SHUTTLE_CONFIG: &str = "～/.shuttle.json";
+
 fn main() {
-    println!("Hello, world!");
+    let path = fs::canonicalize("～/.shuttle.json")?;
+    println!("hello, world! {}", SHUTTLE_CONFIG);
+    println!("path ==> {}", path.display());
 }
